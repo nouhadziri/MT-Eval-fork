77a51c75da4d868dd6aa378dfd891394ce354201
nouhadziri <dziri@ualberta.ca>
2024-08-27 11:05:26 -0400

    debug

--- inference_test.py
+++ inference_test.py
@@ -130,7 +130,129 @@ def main(
     use_gold_history: bool = False,
     n_forward: int = -1,
 ):
-    data = load_dataset("wckwan/MT-Eval", task_name, split="test").to_list()
+    task_type, task_subtype = task_name.split("_", 1)
+    if "ablation" in task_name:
+        task_type = "_".join(task_name.split("_")[:2])
+        task_subtype = "_".join(task_name.split("_")[2:])
+        out_filename = os.path.join(
+            INFERENCE_OUTPUT,
+            task_type,
+            f"{task_subtype}_{model_name}.jsonl",
+        )
+    elif use_gold_history and "ablation" not in task_name:
+        out_filename = os.path.join(
+            INFERENCE_OUTPUT,
+            task_type,
+            f"{task_subtype}_gold_{model_name}.jsonl",
+        )
+    else:
+        out_filename = os.path.join(
+            INFERENCE_OUTPUT,
+            task_type,
+            f"{task_subtype}_{model_name}.jsonl",
+        )
+    logger = get_logger(
+        name=__name__,
+        console_level="info",
+        file_level="debug",
+        log_path=os.path.join(
+            "log",
+            f"{task_name}_{model_name}.log",
+        ),
+        maxBytes=10000000,
+    )
+
+    if not end_tokens:
+        end_tokens = config[model_name]["end_tokens"]
+        logger.info(f"Changed end_tokens to {end_tokens}")
+    if "ablation" in task_name:
+        data = [
+            json.loads(row)
+            for row in open(os.path.join("data", f"{task_name}.jsonl"))
+        ]
+    else:
+        data = load_dataset(
+            "wckwan/MT-Eval", task_name, split="test"
+        ).to_list()
+    out_data = []
+    if out_filename and os.path.exists(out_filename):
+        out_data = [json.loads(line) for line in open(out_filename)]
+
+    print_first_prompt = False
+    total_forward = sum(
+        turn["do_inference"] for row in data for turn in row[conv_key]
+    )
+    if resume and out_data:
+        matched = 0
+        ori_row_map = {
+            f"{row['id']}#{turn['id']}": turn
+            for row in data
+            for turn in row[conv_key]
+        }
+        for row in out_data:
+            for turn in row[conv_key]:
+                if not turn["do_inference"]:
+                    continue
+                _key = f"{row['id']}#{turn['id']}"
+                if output_key in turn and _key in ori_row_map:
+                    ori_row_map[_key][output_key] = turn[output_key]
+                    matched += 1
+        if total_forward == matched:
+            print(f"{out_filename} has finished.")
+            return
+
+        logger.info(f"Resumed {matched} instances from {out_filename}.")
+
+    os.makedirs(os.path.dirname(out_filename), exist_ok=True)
+    use_openai = False
+    model_path = config[model_name]["path"]
+    if "gpt-3.5" in model_name or "gpt-4" in model_name:
+        use_openai = True
+    else:
+        use_flash = config[model_name]["use_flash_attn"] and FLASH_AVAILABLE
+        if use_flash:
+            logger.info("Using flash attention2.")
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            device_map="auto",
+            load_in_8bit=load_8bit,
+            torch_dtype=torch.float16,
+            trust_remote_code=True,
+            attn_implementation="flash_attention_2" if use_flash else None,
+            **load_model_args,
+        )
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_path, trust_remote_code=True
+        )
+        tokenizer.padding_side = "left"
+        try:
+            tokenizer.pad_token_id = tokenizer.eos_token_id
+        except AttributeError as e:
+            logger.info(e)
+            logger.info(
+                "Can't set the tokenizer.pad_token_id but it's probably"
+                " ok if the model is chatglm."
+            )
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            max_new_tokens=max_new_tokens,
+            bos_token_id=tokenizer.bos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+            pad_token_id=tokenizer.pad_token_id,
+            do_sample=do_sample,
+        )
+    logger.info(f"loaded model `{model_name}`")
+    if n_forward > 0:
+        logger.info(f"Only inference on the first {n_forward} examples.")
+        pbar = tqdm(total=n_forward)
+    else:
+        pbar = tqdm(total=total_forward)
+    pbar.set_description(f"Inferencing {out_filename}")
+    token_per_second_list = []
+
+
     prompts = []
     for i, row in enumerate(data):
         # create prompt
@@ -171,7 +293,6 @@ def main(
     # tokenizer = AutoTokenizer.from_pretrained(
     #     model_path, trust_remote_code=True
     # )
-    breakpoint()
     outputs = vllm_generate(model=model, prompts=prompts)
     generations_dir = "/net/nfs.cirrascale/mosaic/nouhad/projects/MT-Eval-fork/inference_outputs/refinement"
     out_file_path = os.path.join(generations_dir, f"preds_{dataset}.json")
